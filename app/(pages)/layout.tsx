c2a7bd1dee69f6115c457bc2d217954a49c14a10
Codin-Technologies <codintechnologies@gmail.com>
2025-11-24 20:05:09 +0300

    add sidebar wrapper

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -1,20 +1,11 @@
-import dynamic from "next/dynamic";
 import Header from "@/components/header";
+import SidebarWrapper from "@/components/navigation/SidebarWrapper";
 import { ReactNode } from "react";
-
-// Dynamically import SidebarNav as a client component
-const SidebarNav = dynamic(
-  () => import("@/components/navigation/sidebarNav"),
-  {
-    ssr: false, // disable server-side rendering
-    loading: () => <div className="w-64 bg-white flex items-center justify-center">Loading sidebar...</div>, // optional fallback
-  }
-);
 
 const TMSLayout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="flex flex-col h-screen bg-gray-100">
-      {/* Top header */}
+      {/* Header */}
       <div className="h-[7vh] flex">
         <div className="w-64 bg-white flex items-center justify-center">
           <h2 className="text-2xl font-bold text-black">TMS Navigation</h2>
@@ -23,8 +14,7 @@ const TMSLayout = ({ children }: { children: ReactNode }) => {
           <Header
             user={{
               name: "Moody",
-              avatar:
-                "https://img.freepik.com/free-photo/portrait-man-cartoon-style_23-2151133887.jpg?t=st=1762602947~exp=1762606547~hmac=8cdcd490bf0ac80315488b720abda108a1f219a52c998d682b9334b1bd5478c4&w=2000",
+              avatar: "https://img.freepik.com/free-photo/portrait-man-cartoon-style_23-2151133887.jpg",
             }}
           />
         </div>
@@ -32,7 +22,7 @@ const TMSLayout = ({ children }: { children: ReactNode }) => {
 
       {/* Sidebar + main content */}
       <div className="h-[93vh] flex">
-        <SidebarNav />
+        <SidebarWrapper /> {/* client component */}
         <div className="p-8 h-[93vh] overflow-auto flex-1">{children}</div>
       </div>
     </div>
@@ -40,4 +30,3 @@ const TMSLayout = ({ children }: { children: ReactNode }) => {
 };
 
 export default TMSLayout;
-
---
+++ components/navigation/SideBarWrapper.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import SidebarNav from "./sidebarNav";
+
+export default function SidebarWrapper() {
+  return <SidebarNav />;
+}
